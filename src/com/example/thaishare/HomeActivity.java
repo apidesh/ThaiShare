3a1099e4d300da581ca13b8d7242d8b2f1ba815a
Jitrapon Tiachunpun <yoshi3003@gmail.com>
2014-03-23 20:33:33 +0700

    update main menu

---
+++ src/com/example/thaishare/HomeActivity.java
@@ -0,0 +1,40 @@
+package com.example.thaishare;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class HomeActivity extends Activity {
+	
+	private Button quickModeBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        
+        Toast.makeText(HomeActivity.this,"Welcome to ThaiShare!", Toast.LENGTH_SHORT).show();
+        
+        // set up button listener
+        quickModeBtn = (Button) findViewById(R.id.quickModeBtn);
+        quickModeBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Toast.makeText(HomeActivity.this,"Navigating to quick mode", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+    
+}
